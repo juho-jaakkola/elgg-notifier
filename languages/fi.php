a0edfd2e1eae673f5264791dfa4f9ff6eb04fc61
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2012-12-17 14:36:01 +0200

    Added finnish translations.

--- languages/fi.php
+++ languages/fi.php
@@ -2,6 +2,17 @@
 
 $lang = array(
 	'notifier:' => '',
+	'notifier:notification' => 'Ilmoitus',
+	'notifier:notifications' => 'Ilmoitukset',
+	'notifier:view:all' => 'Näytä kaikki',
+	'notifier:all' => 'Kaikki ilmoitukset',
+	'notifier:none' => 'Ei ilmoituksia',
+	'notifier:unreadcount' => 'Lukemattomat ilmoitukset (%s)',
+	'notification:method:notifier' => 'Ilmoitukset',
+
+	// Error messages
+	'notifier:error:not_found' => 'Ilmoitusta ei löytynyt',
+	'notifier:error:target_not_found' => 'Ilmoituksen kohdetta ei löytynyt, joten se on todennäköisesti poistettu.',
 );
 
 add_translation('fi', $lang);
