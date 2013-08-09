aa7214ce0dd5399d7351d7da9b52f77268f4884d
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-08-09 16:14:31 +0300

    Marks "new friend" notification read when viewing the user's profile.

--- views/default/notifier/view_listener.php
+++ views/default/notifier/view_listener.php
@@ -6,17 +6,30 @@
  * @uses $vars['entity_guid'] An elgg entity guid that may be used instead of $vars['entity'] 
  */
 
-$target_guid = (get_input('entity_guid')) ? (get_input('entity_guid')) : ($vars['entity']->guid);
-
-$user_guid = elgg_get_logged_in_user_guid();
-
 $override = false;
+
+$options = array(
+	'type' => 'object',
+	'subtype' => 'notification',
+	'owner_guid' => elgg_get_logged_in_user_guid(),
+	'relationship' => 'hasObject', // TODO Use constant
+	'inverse_relationship' => true,
+);
 
 if (elgg_in_context('profile')) {
 	// User profile doesn't have a full view so override the full_view check
 	$override = true;
 	// Parameters are not available so use page owner as target
-	$target_guid = elgg_get_page_owner_guid();
+	$options['relationship_guid'] = elgg_get_page_owner_guid();
+
+	// In notification "You have a new friend" the actor is the new friend
+	$options['relationship'] = 'hasActor'; // TODO Use constant
+	$options['metadata_name_value_pairs'] = array(
+		'name' => 'event',
+		'value' => 'create:relationship:friend'
+	);
+} else {
+	$options['relationship_guid'] = (get_input('entity_guid')) ? (get_input('entity_guid')) : ($vars['entity']->guid);
 }
 
 // Thewire doesn't have a full view so override the full_view check
@@ -25,16 +38,9 @@ if ($vars['entity'] && $vars['entity']->getSubtype() == 'thewire') {
 }
 
 // Mark notification read only if user is looking at the full view
-if ($target_guid && ($vars['full_view'] || $vars['full'] || $override)) {
+if ($options['relationship_guid'] && ($vars['full_view'] || $override)) {
 	// Get notifications related to the entity
-	$notifications = elgg_get_entities_from_relationship(array(
-		'type' => 'object',
-		'subtype' => 'notification',
-		'owner_guid' => $user_guid,
-		'relationship_guid' => $target_guid,
-		'relationship' => 'hasObject',
-		'inverse_relationship' => true,
-	));
+	$notifications = elgg_get_entities_from_relationship($options);
 
 	// Mark all the notifications related to this entity as "read"
 	if ($notifications) {
