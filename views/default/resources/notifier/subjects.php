a7535365cc1801f9c5dbc673af48ce7d8a2bb9a8
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-01-25 16:14:35 +0100

    fix(resources): better instance validation and formatting in subjects resource

--- views/default/resources/notifier/subjects.php
+++ views/default/resources/notifier/subjects.php
@@ -1,19 +1,20 @@
 <?php
+
 /**
  * Display a list of notification subjects
  */
 
+elgg_ajax_gatekeeper();
+
 $guid = elgg_extract('guid', $vars);
 $notification = get_entity($guid);
 
-if ($notification) {
-	$content = elgg_view_entity_list($notification->getSubjects());
-} else {
-	$content = elgg_echo('noaccess');
+if (!$notification instanceof ElggNotification) {
+	forward('', '403');
 }
 
-echo <<<HTML
-<div class="notifier-lightbox-wrapper">
-	$content
-</div>
-HTML;
+$content = elgg_view_entity_list($notification->getSubjects());
+
+echo elgg_format_element('div', [
+	'class' => 'notifier-lightbox-wrapper',
+		], $content);
